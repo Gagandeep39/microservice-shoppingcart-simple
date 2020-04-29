baaba64a9c7d8c75debc614fa358d44353292b5b
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-29 13:02:37 +0530

    removed hystrix

--- microservice3-frontend-service/src/main/java/com/gagan/microservice3frontendservice/service/CartServiceImpl.java
+++ microservice3-frontend-service/src/main/java/com/gagan/microservice3frontendservice/service/CartServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import com.gagan.microservice3frontendservice.model.Cart;
 import com.gagan.microservice3frontendservice.model.Product;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,46 +27,27 @@ public class CartServiceImpl implements CartService {
     @Autowired
     private ProductService productService;
 
-    @Autowired
-    private Cart cart;
-
     @Value("${service.url.cart}")
     private String cartUrl;
 
     Logger logger = LoggerFactory.getLogger(CartServiceImpl.class);
 
     @Override
-    @HystrixCommand(fallbackMethod = "fallbackCreateCart")
     public Cart createCart() {
         return restTemplate.postForObject(cartUrl + "/carts", "", Cart.class);
     }
 
-    public Cart fallbackCreateCart() {
-        return cart;
-    }
-
     @Override
-    @HystrixCommand(fallbackMethod = "fallbackAddItemToCart")
     public Cart addItemToCart(Integer cartId, Product product) {
         return restTemplate.postForObject(cartUrl + "/carts/" + cartId, product, Cart.class);
     }
 
-    public Cart fallbackAddItemToCart(Integer cartId, Product product) {
-        return cart;
-    }
-
     @Override
-    @HystrixCommand(fallbackMethod = "fallbackRemoveItemFromCart")
     public Cart removeItemFromCart(Integer cartId, Integer productId) {
         return restTemplate.postForObject(cartUrl + "/carts/" + cartId + "/" + productId, "", Cart.class);
     }
 
-    public Cart fallbackRemoveItemFromCart(Integer cartId, Integer productId) {
-        return cart;
-    }
-
     @Override
-    @HystrixCommand(fallbackMethod = "fallbackCheckout")
     public Cart checkout(Cart cart) {
         for (Product product : cart.getProducts()) {
             productService.updateStocks(product.getId(), 1);
@@ -75,18 +55,9 @@ public class CartServiceImpl implements CartService {
         return fetchById(cart.getCartId());
     }
 
-    public Cart fallbackCheckout(Cart cart) {
-        return cart;
-    }
-
     @Override
-    @HystrixCommand(fallbackMethod = "fallbackFetchById")
     public Cart fetchById(Integer cartId) {
         return restTemplate.getForObject(cartUrl + "/carts/" + cartId, Cart.class);
-    }
-
-    public Cart fallbackFetchById(Integer cartId) {
-        return cart;
     }
 
 }
